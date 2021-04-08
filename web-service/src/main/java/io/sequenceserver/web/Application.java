b7d90985b26b13841ee880ad41b663115fbaa045
sebastian-mrozek <sebastian.mrozek@gmail.com>
2021-04-08 12:00:39 +1200

    Refactor application to allow instantiation in tests

--- web-service/src/main/java/io/sequenceserver/web/Application.java
+++ web-service/src/main/java/io/sequenceserver/web/Application.java
@@ -4,9 +4,32 @@ import io.javalin.Javalin;
 
 public class Application {
 
+    private final Javalin server;
+
     public static void main(String[] args) {
-        Javalin server = Javalin.create();
+        var app = new Application();
+        app.start();
+    }
+
+    public Application() {
+        this.server = Javalin.create();
         WebRoutesRegistration.init(server);
+        registerExceptionMappers();
+    }
+
+    public void start() {
         server.start();
+    }
+
+    public void start(int port) {
+        server.start(port);
+    }
+
+    public void stop() {
+        server.stop();
+    }
+
+    private void registerExceptionMappers() {
+//        server.exception(
     }
 }
